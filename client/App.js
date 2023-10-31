9a4cba17dbaf31bdeb70b84eac22bd9c984009c2
Palc3301 <pedroarruda8@live.com>
2023-10-31 17:26:01 -0300

    Banner Scroll, Footer with icons & Product card with add/details function

--- client/App.js
+++ client/App.js
@@ -1,24 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home  from './screens/Home';
-import About from './screens/About';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./screens/Home";
+import About from "./screens/About";
+import ProductDetails from "./screens/ProductDetails";
 
 //routes
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="home" component={Home} 
-        options={{
-          headerShown: false,
-          }}/>
-           <Stack.Screen name="mobile" component={About} />
+        <Stack.Screen
+          name="home"
+          component={Home}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen name="productDetails" component={ProductDetails} />
+        <Stack.Screen name="mobile" component={About} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
--- client/screens/Home.js
+++ client/screens/Home.js
@@ -2,24 +2,28 @@ import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import Layout from "../components/Layout/Layout";
 import Categories from "../components/Category/Categories";
+import Banner from "../components/Banner/Banner";
+import Products from "../components/Products/Products";
+import Header from "../components/Layout/Header";
 
 const Home = () => {
   return (
     <Layout>
-      <Categories/>
-      <View>
-        <Text>Home Page</Text>
-      </View>
+      <Header />
+      <Categories />
+      <Banner />
+      <Products />
     </Layout>
   );
 };
 
+export default Home;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
-
-export default Home;
