8e1c24e01eabf53e834e620c6db3e9a6b45dd050
Palc3301 <pedroarruda8@live.com>
2023-11-01 21:39:09 -0300

    Cart/PLogin/Register/Account/Payments Screens, add more json datas & Form to InputBox

---
+++ client/data/userData.js
@@ -0,0 +1,10 @@
+export const userData = {
+    _id: 1,
+    profilePic: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+    name: "Arruda",
+    email: "t@t.com",
+    password: "1234567890",
+    address: "Brazil",
+    city: "Campina Grande",
+    contact: "(83) 998384537",
+  };
